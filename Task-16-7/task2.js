52ced62620c3d2ac73c7d7daa1054978f994b1ba
SereenQamhia <Sereen.qamhia@gmail.com>
2023-07-17 23:27:54 +0300

    Added tasks

--- Task-16-7/task2.js
+++ Task-16-7/task2.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById("form1");
-    
     form.addEventListener("submit", function(event) {
       event.preventDefault();
-  
+ 
   
       const name = document.getElementById("name").value;
       const age = document.getElementById("age").value;
@@ -17,6 +16,7 @@ document.addEventListener("DOMContentLoaded", function() {
       const jsLang = document.getElementById("js").checked;
       const siblingsNumber = document.getElementById("sbilings").value;
       const siblingsDescription = document.getElementById("Desc").value;
+    
   
       const card1 = {
         name,
@@ -37,20 +37,55 @@ document.addEventListener("DOMContentLoaded", function() {
       };
   
       const card3 = {
-        number: siblingsNumber,
-        description: siblingsDescription
+      siblingsNumber,
+      siblingsDescription
       };
-   
-      localStorage.setItem("card1", JSON.stringify(card1));
-      localStorage.setItem("card2", JSON.stringify(card2));
+     
+      localStorage.setItem("card1", JSON.stringify( card1 ));
+      localStorage.setItem("card2",JSON.stringify( card2));
       localStorage.setItem("card3", JSON.stringify(card3));
-  
+    
+    
     
       form.reset();
   
+     let c1=document.getElementById("card1");
+     c1.style="width :200px ; height : 200px ; border : 1px solid black ; margin :20px ; padding :5px;"
+     let c2=document.getElementById("card2");
+     c2.style="width :200px ; height : 200px ; border : 1px solid black ; margin :20px ; padding :5px ;"
+     let c3=document.getElementById("card3");
+     c3.style="width :200px ; height : 200px ; border : 1px solid black ; margin :20px ; padding :5px ;"
+      
+     
+
+      let x = JSON.parse(localStorage.getItem("card1"))
+      c1.innerHTML= "name=" +x.name +`<br>`+ "age="  + x.age + `<br>` + "gender=" + x.gender + `<br>`+ "image :" + x.image + `<br>` + "birthday=" + x.birthday 
+     
+      let z = JSON.parse(localStorage.getItem("card2"));
+      function langs(x) {
+        let m = '';
+      
+        if (x.html) {
+          m += 'HTML  ';
+        }
+        if (x.css) {
+          m += 'CSS  ';
+        }
+        if (x.js) {
+          m += 'JS';
+        }
+      
+        return m;
+      }
+      c2.innerHTML= "description=" + z.description + `<br>`+ "major=" + z.major + `<br>`+ "languages=" +langs(z.languages)
+      
+      let i = JSON.parse(localStorage.getItem("card3"))
+      c3.innerHTML= " siblingsNumber=" + i.siblingsNumber +`<br>`+ "siblingsDescription :" + i.siblingsDescription 
+
    
-      console.log(card1);
-      console.log(card2);
-      console.log(card3);
-    });
-  });
+     
+
+  })
+})
+   
+
--- Task-16-7/task3.js
+++ Task-16-7/task3.js
@@ -1,1 +1,69 @@
+let fname = document.getElementById("fname");
+let lname = document.getElementById("lname");
+let bdate = document.getElementById("bdate");
+let email = document.getElementById("email");
+let confemail = document.getElementById("confemail");
+let pass = document.getElementById("pass");
+let confpass= document.getElementById("confpass");
+fname.addEventListener("input", check)
+lname.addEventListener("input", check1)
+bdate.addEventListener("input", check2)
+email.addEventListener("input", check3)
+confemail.addEventListener("input", check4)
+pass.addEventListener("input", check5)
+confpass.addEventListener("input", check6)
 
+
+
+function check() {
+    let r = /^[^\d]+$/
+     if (r.test(fname.value)){
+     document.getElementById('text').innerHTML= "correct name"
+     }
+     else { document.getElementById('text').innerHTML= "numbers are not allowed" }
+}
+function check1() {
+    let r = /^[^\d]+$/
+     if (r.test(lname.value)){
+     document.getElementById('text1').innerHTML= "correct name"
+     }
+     else { document.getElementById('text1').innerHTML= "numbers are not allowed" }
+}
+function check2() {
+    let r = /^[0-9]{1,2}[-]{1}[0-9]{1,2}[-]{1}[0-9]{4}$/;
+     if (r.test(bdate.value)){
+     document.getElementById('text2').innerHTML= "correct date"
+     }
+     else { document.getElementById('text2').innerHTML= "the format should be dd-mm-yyyy" }
+}
+function check3() {
+    let r = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+     if (r.test(email.value)){
+     document.getElementById('text3').innerHTML= "Email is valid"
+     }
+     else { document.getElementById('text3').innerHTML= "Email is not valid - must be like example@gmail.com" }
+}
+function check4() {
+    let x = email.value;
+    if (x.match(confemail.value)){
+        document.getElementById('text4').innerHTML = "Email is valid";
+    } else {
+        document.getElementById('text4').innerHTML = "Email is not valid - try again";
+    }
+}
+function check5() {
+    let r = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+     if (r.test(pass.value)){
+     document.getElementById('text5').innerHTML= "pass is valid"
+     }
+     else { document.getElementById('text5').innerHTML= "pass is not valid -try again" }
+}
+
+function check6() {
+    let x = pass.value;
+    if (x.match(confpass.value)){
+        document.getElementById('text6').innerHTML = "Password is valid";
+    } else {
+        document.getElementById('text6').innerHTML = "Password is not valid - try again";
+    }
+}
